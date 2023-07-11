3c74daed79ebe82e2169d4a36aa6b3840226cfc7
Amanda Hilton <114438874+Amandapanda-hub@users.noreply.github.com>
2023-07-10 22:19:56 -0600

    finaglling hero/footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -10,13 +10,13 @@ const Footer = () => {
     <div className="flex flex-col items-center justify-center w-full min-h-screen bg-[#F5F3F4] p-8 f-background">
       <div className="flex flex-col items-center justify-center space-y-8 w-full lg:w-1/2 mt-auto mb-auto pt-[7rem] ">
         <h1 className="text-3xl font-semibold tracking-wider leading-8 text-center">Sign Up To Make A Change</h1>
-        <p className='text-2xl font-normal text-center tracking-wide w-full lg:w-[60%]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
+        <p className='text-2xl font-normal text-center tracking-wide w-full lg:w-[60%] sm:text-xl'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
         <div className='flex space-x-9 justify-center lg:justify-start'>
           <button className="px-8 py-2 bg-[#DFAB90] text-white rounded">Sign Up</button>
           <button className="px-8 py-2 bg-[#3F2305] text-white rounded">Log In</button>
         </div>
       </div>
-      <div className="flex flex-col items-center justify-center space-y-8 w-full lg:w-1/2 mt-auto mb-auto text-2xl">
+      <div className="flex flex-col items-center justify-center space-y-8 sm:space-y-10 w-full lg:w-1/2 mt-auto mb-auto text-2xl">
         <div className='w-20 h-20 rounded-full border border-[#3F2305] flex items-center justify-center'>
           <MdOutlineNaturePeople className="w-[50%] h-[50%] text-[#3F2305]"/>
         </div>
@@ -26,14 +26,17 @@ const Footer = () => {
           <img src={GooglePlay} alt="Google Play Store" className="w-40 h-auto"/>
         </div>
       </div>
-      <div className="flex justify-around w-full lg:w-[50%] mt-auto mb-10 text-sm sm:text-base text-[#3F2305]">
-        <Link className="" to="/link1">About Us</Link>
-        <Link className="" to="/link2">Services</Link>
-        <Link className="" to="/link3">Shop</Link>
-        <Link className="" to="/link4">Donate</Link>
-        <Link className="" to="/link5">Contact Us</Link>
-        <Link className="" to="/link6">Privacy Policy</Link>
-      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-6 gap-4 justify-items-center w-full lg:w-[50%] mt-auto mb-10 text-sm sm:text-base text-[#3F2305]">
+    <Link className="" to="/link1">About Us</Link>
+    <Link className="" to="/link2">Services</Link>
+    <Link className="" to="/link3">Shop</Link>
+    <Link className="" to="/link4">Donate</Link>
+    <Link className="" to="/link5">Contact Us</Link>
+    <Link className="" to="/link6">Privacy Policy</Link>
+</div>
+
+
+
       <p className="text-center mb-4">&copy; {new Date().getFullYear()} BLISSFUL BLOOMS.</p>
     </div>
   );
--- src/pages/Home/Hero/Hero.jsx
+++ src/pages/Home/Hero/Hero.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import FlowerOutline from '../../../assets/images/blockyflower.png';
 import { TypeAnimation } from 'react-type-animation';
+import "./HeroStyles.css";
 
 export default function Hero() {
   return (
     <div className="flex flex-col lg:flex-row">
-      <div className="w-full lg:w-1/3 bg-[#F5F3F4] flex-grow flex flex-col items-center justify-center space-y-10">
-        <h1 className="text-6xl font-bold text-[#4F5E4E] bg-clip-text">
+      <div className="w-full lg:w-1/3 bg-[#F5F3F4] flex-grow flex flex-col items-center justify-center space-y-10 sm:pb-5">
+        <h1 className="sm:text-3xl md:text-4xl text-5xl font-bold text-[#4F5E4E] bg-clip-text">
           One Seed
         </h1>
-        <div className="bg-[#F5F3F4] "> 
-        <h1 className="text-5xl font-bold text-[#DFAB90] bg-clip-text overlay">
+        <div className="items-center justify-center bg-[#F5F3F4] sm:text-2xl sm:mt-3"> 
+        <h1 className="text-5xl md:text-3xl font-bold text-[#DFAB90] bg-clip-text overlay sm:pb-5 md:pb-10 lg:pb-0">
             <TypeAnimation
                 sequence={[
                     'Brings Joy',
@@ -22,8 +23,8 @@ export default function Hero() {
                 ]}
                 wrapper = 'span'
                 speed={30}
-                className="outline-text"
-                style={{ fontSize: '3rem', display: 'inline-block', background: '#F5F3F4'}}
+                className="outline-text type-animation-text"
+                style={{ fontSize: '2rem', display: 'inline-block', background: '#F5F3F4'}}
                 repeat={Infinity}
                 />
         </h1>
